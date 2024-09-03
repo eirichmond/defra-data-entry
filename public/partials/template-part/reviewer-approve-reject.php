9e6b5cca6709802e4bcefcfd9c6019988e793a28
Elliott Richmond <elliott@squareonemd.co.uk>
2024-09-03 16:19:01 +0100

    related to bug90

--- public/partials/template-part/reviewer-approve-reject.php
+++ public/partials/template-part/reviewer-approve-reject.php
@@ -1,8 +1,13 @@
 <div class="mb-3">
 
-    <?php if( $count == 1 && $status[0] == '20' || $status[0] == '30' ) { ?>
-        <button id="approved-by-dr" class="btn btn-success" type="submit" name="status" value="approve">Approve</button>
-        <button id="rejected-by-dr" class="btn btn-danger" type="submit" name="status" value="reject">Reject</button>
+
+    <?php if( !isset($revoked) ) { ?>
+
+        <?php if( $count == 1 && $status[0] == '20' || $status[0] == '30' ) { ?>
+            <button id="approved-by-dr" class="btn btn-success" type="submit" name="status" value="approve">Approve</button>
+            <button id="rejected-by-dr" class="btn btn-danger" type="submit" name="status" value="reject">Reject</button>
+        <?php } ?>
+        
     <?php } ?>
     
     <?php if( in_array( '600', $status ) && empty($revoked[0]) ) { ?>
